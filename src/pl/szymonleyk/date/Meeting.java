0c30f8d977b3a47022a209f3b5e998353b974a75
Szymon Leyk <leyas@wp.pl>
2020-01-16 13:06:49 +0100

    #34 przykład z datami, zapis do pliku, dodanie meetingów

---
+++ src/pl/szymonleyk/date/Meeting.java
@@ -0,0 +1,40 @@
+package pl.szymonleyk.date;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import obiektowosc.Student;
+
+public class Meeting {
+	private LocalDateTime dateTime;
+	private Student student;
+
+	public Meeting(LocalDateTime dateTime, Student student) {
+		super();
+		this.dateTime = dateTime;
+		this.student = student;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime date) {
+		this.dateTime = date;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return  student + "\t|\t" + dateTime.format(DateTimeFormatter.ofPattern("dd MMM Y\t|\tHH:mm", Locale.forLanguageTag("cy")));
+	}
+
+}
